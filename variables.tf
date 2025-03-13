66cd39208c0f2961043caffe1c19e2a51ea32099
jakebark <jakebark@amazon.co.uk>
2025-03-13 15:16:43 +0000

    var validation

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT-0
 
 variable "ou_map" {
+  type = map(string)
 }
 
 variable "policies_directory" {
@@ -12,4 +13,14 @@ variable "policies_directory" {
 variable "policy_type" {
   type    = string
   default = "SERVICE_CONTROL_POLICY"
+  validation {
+    condition = contains([
+      "AISERVICES_OPT_OUT_POLICY",
+      "BACKUP_POLICY",
+      "RESOURCE_CONTROL_POLICY",
+      "SERVICE_CONTROL_POLICY",
+      "TAG_POLICY"
+    ], var.policy_type)
+    error_message = "unsupported policy type"
+  }
 }
